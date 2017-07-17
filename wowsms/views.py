be32229e4c4c9e7dab132788d33cf5e95165c8fd
= <=>
2017-07-16 21:47:32 -0500

    'v1'

--- wowsms/views.py
+++ wowsms/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'index.html')
+
+
+def login(request):
+	return render(request, 'login.html')
+
+def register(request):
+	return render(request, 'register.html')
