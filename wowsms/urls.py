844eebc8fb82a49aa80a6572b2b7ffb7d9c15f36
= <=>
2017-07-17 00:33:52 -0500

    'v1'

--- wowsms/urls.py
+++ wowsms/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'$',views.index, name='index'),
-	url(r'login',views.login, name='login'),
-	url(r'register',views.register, name='register')
+	url(r'^$',views.index, name='index'),
+	url(r'login/$',views.login, name='login'),
+	url(r'register/$',views.register, name='register'),
+	url(r'(?P<user_id>\w+)/dashboard/$', views.dashboard, name='dashboard'),
+	url(r'(?P<user_id>\w+)/pricing/$', views.pricing, name='pricing')
 ]
--- wowsms/views.py
+++ wowsms/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 
 
 def index(request):
 	return render(request, 'index.html')
-
 
 def login(request):
 	return render(request, 'login.html')
 
 def register(request):
 	return render(request, 'register.html')
+
+def dashboard(request, user_id):
+	return render(request, 'dashboard.html', {'user':user_id})
+
+def pricing(request, user_id):
+	return render(request, 'pricing.html', {'user':user_id})
