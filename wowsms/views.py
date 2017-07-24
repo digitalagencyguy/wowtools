cba17fcf3c41510c1e0b12b00d36291f049babf6
= <=>
2017-07-24 00:08:15 -0500

    context processing automatically handled

--- wowsms/views.py
+++ wowsms/views.py
@@ -6,20 +6,16 @@ from .utils import Context
 from .utils import Authenticate
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.views.decorators.http import require_http_methods as methods
 
-@methods(["GET"])
 def index(request):
 	response = Process(request).index('index')
 	return render(request, response.template, response.context)
 
-@methods(["GET", "POST"])
 def audience(request, user_id):
 	if request.method == 'GET':
 		response = Process(request, user_id).main('audience')
 		return render(request, response.template, response.context)
 
-@methods(["GET", "POST"])
 def settings(request, user_id):
 	if request.method == 'GET':
 		response = Process(request, user_id).main('settings')
@@ -27,30 +23,25 @@ def settings(request, user_id):
 	elif request.method == 'POST':
 		return redirect('/')
 
-@methods(["GET", "POST"])
 def sequences(request, user_id):
 	if request.method == 'GET':
 		response = Process(request, user_id).main('sequences')
 		return render(request, response.template, response.context)
 
-@methods(["GET", "POST"])
 def campaigns(request, user_id):
 	if request.method == 'GET':
 		response = Process(request, user_id).main('campaigns')
 		return render(request, response.template, response.context)
 
-@methods(["GET", "POST"])
 def sms(request, user_id):
 	if request.method == 'GET':
 		response = Process(request, user_id).main('sms')
 		return render(request, response.template, response.context)
 
-@methods(["GET"])
 def logout(request):
 	request.session.clear()
 	return redirect('/')
 
-@methods(["GET","POST"])
 def login(request):
 	request.session['user'] = '4224'
 	return redirect('/')
