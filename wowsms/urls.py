9bf7308cc25af5844b34815db877c60ca1413828
= <=>
2017-07-25 20:24:34 -0500

    added schedule page template

--- wowsms/urls.py
+++ wowsms/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 	url(r'(?P<user_id>\w+)/sequences/?$', views.sequences, name='sequences'),
 	url(r'(?P<user_id>\w+)/campaigns/?$', views.campaigns, name='campaigns'),
 	url(r'(?P<user_id>\w+)/sms/?$', views.sms, name='sms'),
+	url(r'(?P<user_id>\w+)/schedule/?$', views.schedule, name='schedule'),
 	url(r'me?/$', views.me, name='me'),
 	url(r'logout/?$',views.logout, name='logout'),
 	url(r'login/?$',views.login, name='login')
