e94baedf2abb29b111b2b1ba1ca3b740ce8198dc
= <=>
2017-07-28 05:11:37 +0000

    Added approved host

--- wowtools/settings.py
+++ wowtools/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'n!&+!y85!&r4$k7kbgxp8z_2%b@a(^*lxmhwdqqh6c#j$pu42d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['qtxr.pythonanywhere.com']
+ALLOWED_HOSTS = ['qtxr.pythonanywhere.com','wowbot.me','www.wowbot.me']
 
 
 # Application definition
