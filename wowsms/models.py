ed193e648ea67e6f6a3e0f6dd370abe20e1b612d
= <=>
2017-07-29 17:20:56 -0500

    added models

--- wowsms/models.py
+++ wowsms/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+
+
+
+class Customer(models.Model):
+
+	firstname = models.TextField()
+	lastname = models.TextField()
+	credit = models.IntegerField()
+	queue = models.IntegerField()
+	trial = models.BooleanField()
+	mobile = models.TextField()
+	businessName = models.TextField()
+	businessAddress = models.TextField()
+	timezone = models.TextField()
+	street1 = models.TextField()
+	street2 = models.TextField()
+	town = models.TextField()
+	zipCode = models.IntegerField()
+	country = models.TextField()
+
+	class Meta:
+
+		db_table = 'customers'
